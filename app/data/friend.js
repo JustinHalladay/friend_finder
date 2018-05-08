85e09bd173e4a8ad42a7a01d2b903e0d7b053511
Justin <justaoiabilly99@yahoo.com>
2018-05-07 19:26:49 -0600

    Got some tweeking to do

---
+++ app/data/friend.js
@@ -0,0 +1,165 @@
+// Data structure that will store the friends data
+var friendsArray = [
+	{
+		name: 'Bushwick Bill',
+		photo: 'https://vignette.wikia.nocookie.net/hip-hop-music/images/c/c4/Bushwick_Bill.jpg/revision/latest?cb=20140311043623',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Cash Money',
+		photo: 'https://scontent-lax3-1.xx.fbcdn.net/v/t31.0-8/12185083_10153649393849857_525632409619196415_o.jpg?_nc_cat=0&oh=1c015e2ccf9a0c711a10c9b5e33cae7b&oe=5B54DAD7',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Jay William Henderson',
+		photo: 'https://f4.bcbits.com/img/0008204120_10.jpg',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Ian McFarland',
+		photo: 'https://img00.deviantart.net/7800/i/2007/318/c/7/ian_mcfarland_by_sureality.jpg',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Shug Emery',
+		photo: 'http://www.seanemery.com/wp-content/uploads/2012/09/sean-home-shot-270x230.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Gandalf',
+		photo: 'https://hobbydb-production.s3.amazonaws.com/processed_uploads/subject_photo/subject_photo/image/26433/1497025863-7706-9080/Gand_large.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Radagast',
+		photo: 'https://i.ytimg.com/vi/eHqAcguwhPc/hqdefault.jpg',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Joe Strummer',
+		photo: 'http://www.clashmusic.com/sites/default/files/styles/article_feature/public/legacy/files/JOE%20STRUMMER.jpg?itok=oZ-AC0AB',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Desmond Dekker',
+		photo: 'https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTE1ODA0OTcxOTc4ODE5MDg1/photo-of-desmond-dekker.jpg',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Alex Désert',
+		photo: 'https://www.celebrity8x10s.com/mm5/graphics/00000001/S/1334.jpg',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = friendsArray;
